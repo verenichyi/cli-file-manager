c239324d453b3a33f7f42f8594cbb9a454ce1028
verenichyi <verenichyi@gmail.com>
2022-12-15 17:55:27 +0300

    fix: directory search in ls

--- src/domains/nwd/ls.js
+++ src/domains/nwd/ls.js
@@ -6,24 +6,45 @@ export const ls = async () => {
 	const cwdEntities = await readdir(cwd, { withFileTypes: true });
 	const files = [];
 	const directories = [];
+	const links = [];
+	const unknown = [];
 
 	for (const cwdEntity of cwdEntities) {
-		const isFile = cwdEntity.isFile();
 		const entity = {
 			Name: cwdEntity.name,
-			Type: isFile ? 'file' : 'directory'
+			Type: 'unknown'
 		};
+		const isDirectory = cwdEntity.isDirectory();
+		const isFile = cwdEntity.isFile();
+		const isSymbolicLink = cwdEntity.isSymbolicLink();
 
-		if (isFile) {
-			files.push(entity);
-		} else {
-			directories.push(entity);
+		switch (true) {
+			case isDirectory: {
+				entity.Type = 'directory';
+				directories.push(entity);
+				break;
+			}
+			case isFile: {
+				entity.Type = 'file';
+				files.push(entity);
+				break;
+			}
+			case isSymbolicLink: {
+				entity.Type = 'link';
+				links.push(entity);
+				break;
+			}
+			default: {
+				unknown.push(entity);
+			}
 		}
 	}
 
 	const identifiedEntities = [
 		...sortByField(directories, 'Name'),
-		...sortByField(files, 'Name')
+		...sortByField(files, 'Name'),
+		...sortByField(links, 'Name'),
+		...sortByField(unknown, 'Name')
 	];
 	console.table(identifiedEntities);
 };
